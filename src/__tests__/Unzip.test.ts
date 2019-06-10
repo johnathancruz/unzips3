d8451c40a184633a9780868922de129e7f0bdc98
Jonata Cruz <jonata@predict.info>
2019-06-10 15:15:14 -0300

    .

--- src/__tests__/Unzip.test.ts
+++ src/__tests__/Unzip.test.ts
@@ -1,48 +1,47 @@
 import { S3Event, S3EventRecord } from 'aws-lambda';
 
 const s3EventRecord: S3EventRecord = {
-    awsRegion: '',
-    eventName: '',
-    eventSource: '',
-    eventTime: '',
-    eventVersion: '',
-    requestParameters: {
-        sourceIPAddress: ''
+  awsRegion: '',
+  eventName: '',
+  eventSource: '',
+  eventTime: '',
+  eventVersion: '',
+  requestParameters: {
+    sourceIPAddress: '',
+  },
+  responseElements: {
+    'x-amz-id-2': '',
+    'x-amz-request-id': '',
+  },
+  s3: {
+    bucket: {
+      arn: '',
+      name: '',
+      ownerIdentity: {
+        principalId: '',
+      },
     },
-    responseElements: {
-        'x-amz-id-2': '',
-        'x-amz-request-id': ''
+    configurationId: '',
+    object: {
+      eTag: '',
+      key: '',
+      sequencer: '',
+      size: 0,
+      versionId: '',
     },
-    s3: {
-        bucket: {
-            arn: '',
-            name: '',
-            ownerIdentity: {
-                principalId: ''
-            }
-        },
-        configurationId: '',
-        object: {
-            eTag: '',
-            key: '',
-            sequencer: '',
-            size: 0,
-            versionId: ''
-        },
-        s3SchemaVersion: ''
-    },
-    userIdentity: {
-        principalId: ''
-    }
+    s3SchemaVersion: '',
+  },
+  userIdentity: {
+    principalId: '',
+  },
 };
 
 const records: S3EventRecord[] = [s3EventRecord];
 
 const s3Event: S3Event = {
-    Records: records
-}
+  Records: records,
+};
 
 describe('Unzip S3 Test', () => {
-    test('Create IFileToUnzip', () => {
-    });
+  test('Create IFileToUnzip', () => {});
 });
